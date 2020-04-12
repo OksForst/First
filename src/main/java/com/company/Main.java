9e77305ad3d4d89011c27fc4c2f1b6521d409bfa
Оксана <oks-forst89@yandex.ru>
2020-04-12 22:00:26 +0700

    Variable names changed, extra lines removed

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -8,30 +8,27 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер таблицы:");
         if (in.hasNextInt()== false)
-        {
             System.out.println("Запустите программу еще раз и введите целое положительное число");
-        } else {
-            int sizetable = in.nextInt();
+        else {
+            int tableSize = in.nextInt();
             in.close();
 
-            if (sizetable > 0) {
+            if (tableSize > 0) {
                 int size = 32 * 32;
-                String sizeformat = String.valueOf(size);
-                int lenghtformat = sizeformat.length();
-                String str = "-";
-                String str1 = "+";
-                String separator = (str.repeat(lenghtformat) + str1).repeat(sizetable - 1)+ str.repeat(lenghtformat);
-                for (int i = 0; i < sizetable; i++) {
-                    for (int j = 0; j < sizetable; j++) {
+                String formatSize = String.valueOf(size);
+                int format = formatSize.length();
+                String separator = ( "-".repeat(format) + "+").repeat(tableSize - 1)+ "-".repeat(format);
+                for (int i = 0; i < tableSize; i++) {
+                    for (int j = 0; j < tableSize; j++) {
                         int value = (i + 1) * (j + 1);
-                        String tf = "%" + lenghtformat + "d" + "|";
-                        String tf1 = "%" + lenghtformat + 'd' + " ";
-                        if (j < sizetable - 1) {
+                        String tf = "%" + format + "d" + "|";
+                        String tf1 = "%" + format + 'd' + " ";
+                        if (j < tableSize - 1) {
                             System.out.printf(tf, value);
                         } else System.out.printf(tf1, value);
                     }
                     System.out.println();
-                    if (i < sizetable - 1) {
+                    if (i < tableSize - 1) {
                         System.out.println(separator);
                     }
                 }
