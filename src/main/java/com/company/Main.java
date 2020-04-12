332a2222ee09515026b93bd47638576ad61f9ff3
Оксана <oks-forst89@yandex.ru>
2020-04-12 22:17:59 +0700

    Variable names changed, extra lines removed

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,25 +7,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер таблицы:");
-        if (in.hasNextInt()== false)
+        if (!in.hasNextInt()) {
             System.out.println("Запустите программу еще раз и введите целое положительное число");
-        else {
+        } else {
             int tableSize = in.nextInt();
             in.close();
 
             if (tableSize > 0) {
-                int size = 32 * 32;
-                String formatSize = String.valueOf(size);
-                int format = formatSize.length();
-                String separator = ( "-".repeat(format) + "+").repeat(tableSize - 1)+ "-".repeat(format);
+                int format = 4;
+                String separator = ("-".repeat(format) + "+").repeat(tableSize - 1) + "-".repeat(format);
                 for (int i = 0; i < tableSize; i++) {
                     for (int j = 0; j < tableSize; j++) {
                         int value = (i + 1) * (j + 1);
                         String tf = "%" + format + "d" + "|";
-                        String tf1 = "%" + format + "d" + " ";
+                        String tf1 = "%" + format + "d" + ' ';
                         if (j < tableSize - 1) {
                             System.out.printf(tf, value);
-                        } else System.out.printf(tf1, value);
+                        } else
+                            System.out.printf(tf1, value);
                     }
                     System.out.println();
                     if (i < tableSize - 1) {
