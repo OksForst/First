dc5e98b887b0e8fbdf7e5d5ea2394031523f23a5
Оксана <oks-forst89@yandex.ru>
2020-04-14 20:18:36 +0700

    Add methods

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,35 +4,23 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void setTableSize(){
+    Scanner in = new Scanner(System.in);
         System.out.print("Введите размер таблицы:");
-        if (!in.hasNextInt()) {
+        if (!in.hasNextInt())
             System.out.println("Запустите программу еще раз и введите целое положительное число");
-        } else {
-            int tableSize = in.nextInt();
-            in.close();
-
-            if (tableSize > 0) {
-                int format = 4;
-                String separator = ("-".repeat(format) + "+").repeat(tableSize - 1) + "-".repeat(format);
-                for (int i = 0; i < tableSize; i++) {
-                    for (int j = 0; j < tableSize; j++) {
-                        int value = (i + 1) * (j + 1);
-                        String tf = "%" + format + "d" + "|";
-                        String tf1 = "%" + format + "d" + ' ';
-                        if (j < tableSize - 1) {
-                            System.out.printf(tf, value);
-                        } else
-                            System.out.printf(tf1, value);
-                    }
-                    System.out.println();
-                    if (i < tableSize - 1) {
-                        System.out.println(separator);
-                    }
-                }
-            } else System.out.println("Запустите программу еще раз и введите целое положительное число");
-        }
+        else
+    {
+        int tableSize = in.nextInt();
+        in.close();
     }
+    }
+
+    ////////////////////////////////////////////////////
+    public static void main (String[] args) {
+        setTableSize();
+
 }
+}
+
 
