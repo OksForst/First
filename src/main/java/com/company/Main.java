5a2cd0568de91fef6ad677ce54729cc6a104bce9
Оксана <oks-forst89@yandex.ru>
2020-04-17 22:58:41 +0700

    Add methods

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,54 +4,50 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Main {
+
     private static int tableSize;
     private static int format = 4;
+    private static String separator = "";
+    private static Scanner in;
 
-
-
-    public static void getTableSize(){
-        Scanner in = new Scanner(System.in);
+    public static void getTableSize() {
+        in = new Scanner(System.in);
         System.out.print("Введите размер таблицы:");
-        if (!in.hasNextInt()) {
+        if (in.hasNextInt()) {
+            tableSize = in.nextInt();
+            if (tableSize < 1) {
+                getTableSize();
+            }
+        } else {
             System.out.println("Ошибка. Введите число");
             getTableSize();
         }
-        else
-        {
-            tableSize = in.nextInt();
-            in.close();
-        }
     }
 
-
-
-    public static void checkTableSize() {
-        if (tableSize > 0) {
-            String separator = ("-".repeat(format) + "+").repeat(tableSize - 1) + "-".repeat(format);
-            for (int i = 0; i < tableSize; i++) {
-                for (int j = 0; j < tableSize; j++) {
-                    int value = (i + 1) * (j + 1);
-                    String tf = "%" + format + "d" + "|";
-                    String tf1 = "%" + format + "d" + ' ';
-                    if (j < tableSize - 1) {
-                        System.out.printf(tf, value);
-                    } else
-                        System.out.printf(tf1, value);
-                }
-                System.out.println();
-                if (i < tableSize - 1) {
-
-                    System.out.println(separator);
+    public static void printTable() {
+        String separator = ("-".repeat(format) + "+").repeat(tableSize - 1) + "-".repeat(format);
+        for (int i = 0; i < tableSize; i++) {
+            for (int j = 0; j < tableSize; j++) {
+                int value = (i + 1) * (j + 1);
+                String tf = "%" + format + "d" + "|";
+                String tf1 = "%" + format + "d" + ' ';
+                if (j < tableSize - 1) {
+                    System.out.printf(tf, value);
+                } else {
+                    System.out.printf(tf1, value);
                 }
             }
+            System.out.println();
+            if (i < tableSize - 1) {
+                System.out.println(separator);
+            }
         }
-        System.out.println("Запустите программу еще раз и введите целое положительное число");
-        if (tableSize < 1) getTableSize();
-
     }
+
     ////////////////////////////////////////////////////
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         getTableSize();
-        checkTableSize();
+        printTable();
+        in.close();
     }
 }
